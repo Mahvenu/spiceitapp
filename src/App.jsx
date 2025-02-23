52da8074358932dc4d78a2f608f1b08ef965190e
mahvenu <mahvenu@gmail.com>
2025-02-23 18:56:43 +0000

    added sample Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import './customstyles/spicenav.css';
+import Home from './corecomponents/Home';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="card">
+    <Router>
+      <div>
+        <nav>
+          <ul className="spice-horizontal-nav">
+            <li>
+              <Link  to="/">Home</Link>
+            </li>
+            <li>
+              Contact Us
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
         
         <p>
           Product Of VJkamps
         </p>
       </div>
+      </Router>
       
     </>
-  )
+  );
 }
 
 export default App
