0b68a4ea787a0a07f9564c94e7a2e02ff9c00649
mahvenu <mahvenu@gmail.com>
2025-02-23 18:05:41 +0000

    changed the test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-
 import './App.css'
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="card">
+        
+        <p>
+          Product Of VJkamps
+        </p>
+      </div>
       
-      <p className="read-the-docs">
-        Product of VJkamps
-      </p>
     </>
   )
 }
