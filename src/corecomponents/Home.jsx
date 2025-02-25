f60c00b1f6d7ae7029ab02de2465bae78fcbea61
mahvenu <mahvenu@gmail.com>
2025-02-25 22:34:21 +0000

    products load page

--- src/corecomponents/Home.jsx
+++ src/corecomponents/Home.jsx
@@ -1,9 +1,40 @@
+import axios from "axios";
 import React from "react";
-
+import { useEffect, useState } from "react";
 export default function Home () {
+    const [product, setProduct] = useState(null); // State to store product data
+    const [loading, setLoading] = useState(true); // State for loading status
+    const [error, setError] = useState(null); // State for errors
+    
+    useEffect(() => {
+        const fetchProductInformation = async () => {
+            try {
+                const prodInfoResp = await axios.get("https://r46jrehpue.execute-api.ap-south-1.amazonaws.com/spicedev");
+                setProduct(prodInfoResp.data);
+                setLoading(false);
+            } catch (err) {
+                setError(err.message); // Set error state if the request fails
+                setLoading(false); // Set loading to false on error
+            }
+        };
+        fetchProductInformation();
+    },[]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
     return(
-        <div>
-            Home Page Contents
+        <div className="product-info-page">
+            <h1>{product.name}</h1>
+           
+            <p>{product.qty}</p>
+            <p><strong>Price:</strong> ${product.price}</p>
+            
         </div>
     )
 } 
